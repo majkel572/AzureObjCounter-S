5684a5cff8508c0b5308babf96ccd608210f93a5
jgoliszewski <jakub.goliszewski01@gmail.com>
2022-11-24 22:33:49 +0100

    complete send image to blob

--- ObjectCounter/wwwroot/js/site.js
+++ ObjectCounter/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function readImage(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(800)
+                .height(500);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}

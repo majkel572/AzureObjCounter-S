ede6c266383fae72fc67766ccacf4ff81f87a01e
Mikołaj <mrefe12345@gmail.com>
2022-11-28 17:24:49 +0100

    Rewrited python script
    Apparently python script will not run on cloud so had to rewrite it to c# code

---
+++ ObjectCounter/BoundingBox.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureObjCounterS{
+    public class BoundingBox {
+    public double left { get; set; }
+    public double top { get; set; }
+    public double width { get; set; }
+    public double height { get; set; }
+    }
+}
---
+++ ObjectCounter/ResponseJson.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureObjCounterS{
+    public class ResponseJson {
+        public string id { get; set; }
+        public string project { get; set; }
+        public string iteration { get; set; }
+        public DateTime created { get; set; }
+        public List<Predictions> predictions { get; set; }
+    }
+}
